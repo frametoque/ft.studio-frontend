19d47dbbb00ff7afaadcffedefc1d8fd787e4fdf
itsnelitha <itsnelitha@gmail.com>
2026-03-05 11:26:55 +0530

    alive

--- src/app/video-analyzer/lib/keepAlive.js
+++ src/app/video-analyzer/lib/keepAlive.js
@@ -2,7 +2,7 @@
 export const keepAlive = () => {
   setInterval(async () => {
     try {
-      await fetch(`${process.env.NEXT_PUBLIC_API_URL}/`)
+      await fetch(`${process.env.NEXT_PUBLIC_API_URL}`)
     } catch (_) {}
   }, 14 * 60 * 1000) // every 14 minutes
 }
